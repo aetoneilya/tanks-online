d1b0b7d54281359ee9501028fb2f95cf99ef7de9
tloraq <tloraq@student.21-school.ru>
2023-03-25 19:58:52 +0700

    basic drawing done

---
+++ src/TanksClient/src/main/java/module-info.java
@@ -0,0 +1,13 @@
+module tanks.client.tanksclient {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires com.almasb.fxgl.all;
+
+    opens tanks.client.tanksclient to javafx.fxml;
+    exports tanks.client.tanksclient;
+}
